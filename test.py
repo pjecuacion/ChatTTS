a736aca28c09d125e920c65619095fd68ccde22b
pjecuacion <pjecuacion@gmail.com>
2024-06-07 00:09:56 +1000

    test

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
-import torch
-import torchaudio
 import ChatTTS
 from IPython.display import Audio
+import torchaudio
+import torch
+
 
 chat = ChatTTS.Chat()
-chat.load_models(compile=False) # Set to True for better performance
+chat.load_models(compile=True) # Set to True for better performance
 
-texts = ["Hi! My name is Prince",]
 
-wavs = chat.infer(texts, )
+inputs_en = """
+chat T T S is a text to speech model designed for dialogue applications. 
+[uv_break]it supports mixed language input [uv_break]and offers multi speaker 
+capabilities with precise control over prosodic elements [laugh]like like 
+[uv_break]laughter[laugh], [uv_break]pauses, [uv_break]and intonation. 
+[uv_break]it delivers natural and expressive speech,[uv_break]so please
+[uv_break] use the project responsibly at your own risk.[uv_break]
+""".replace('\n', '') # English is still experimental.
 
-torchaudio.save("output1.wav", torch.from_numpy(wavs[0]), 24000)
+params_refine_text = {
+  'prompt': '[oral_5][laugh_5][break_4]'
+} 
+# audio_array_cn = chat.infer(inputs_cn, params_refine_text=params_refine_text)
+audio_array_en = chat.infer(inputs_en, params_refine_text=params_refine_text)
+torchaudio.save("output3.wav", torch.from_numpy(audio_array_en[0]), 24000)
